3183b01e2e282d39f0bf56b78654f66460b599a1
Henrique <henriqueyang17@gmail.com>
2019-10-19 11:49:52 -0300

    error-handling through middleware

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,19 +1,23 @@
 module.exports = (app) => {
-  const create = (req, res) => app.services.accounts.create(req.body)
+  const create = (req, res, next) => app.services.accounts.create(req.body)
     .then((result) => res.status(201).json(result[0]))
-    .catch((err) => res.status(400).json({ error: err.message }));
+    .catch((err) => next(err));
 
-  const findAll = (req, res) => app.services.accounts.findAll()
-    .then((result) => res.status(200).json(result));
+  const findAll = (req, res, next) => app.services.accounts.findAll()
+    .then((result) => res.status(200).json(result))
+    .catch((err) => next(err));
 
-  const find = (req, res) => app.services.accounts.find({ id: req.params.id })
-    .then((result) => res.status(200).json(result));
+  const find = (req, res, next) => app.services.accounts.find({ id: req.params.id })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => next(err));
 
-  const update = (req, res) => app.services.accounts.update(req.params.id, req.body)
-    .then((result) => res.status(200).json(result[0]));
+  const update = (req, res, next) => app.services.accounts.update(req.params.id, req.body)
+    .then((result) => res.status(200).json(result[0]))
+    .catch((err) => next(err));
 
-  const remove = (req, res) => app.services.accounts.remove(req.params.id)
-    .then(() => res.status(204).send());
+  const remove = (req, res, next) => app.services.accounts.remove(req.params.id)
+    .then(() => res.status(204).send())
+    .catch((err) => next(err));
 
   return {
     create,
